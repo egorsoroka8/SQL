d4042466589340a77078f27145acc616d223aa73
Egor <112896404+egorsoroka8@users.noreply.github.com>
2022-10-29 13:53:17 +0300

    Update delivery.sql

--- delivery.sql
+++ delivery.sql
@@ -1,4 +1,3 @@
-````sql
 SELECT first_name AS name, last_name AS surname FROM customers
 JOIN orders ON
 orders.customer_id = customers.customer_id
@@ -7,4 +6,8 @@ orders_products.order_id = orders.order_id
 JOIN products ON
 products.product_id = orders_products.product_id
 WHERE menu_name='SASHIMI SALAD';
-````
+
+"name","surname"
+Jennifer,Radriges
+Bella,Lorenson
+Oscar,Rild
