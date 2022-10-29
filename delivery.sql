24380ea1a251b67d98eb17d60d835779cba53d46
Egor <112896404+egorsoroka8@users.noreply.github.com>
2022-10-29 13:27:58 +0300

    Add files via upload

---
+++ delivery.sql
@@ -0,0 +1,8 @@
+SELECT first_name AS name, last_name AS surname FROM customers
+JOIN orders ON
+orders.customer_id = customers.customer_id
+JOIN orders_products ON
+orders_products.order_id = orders.order_id 
+JOIN products ON
+products.product_id = orders_products.product_id
+WHERE menu_name='SASHIMI SALAD';
