37a8d046a6877d413b4b9c6415f435cb03aeb572
Egor <112896404+egorsoroka8@users.noreply.github.com>
2022-11-01 12:56:29 +0300

    Update world_queries.sql

--- world_queries.sql
+++ world_queries.sql
@@ -144,16 +144,107 @@ ORDER BY
 |Greenland|    0.0259|
 
 
-#7 Display country and city names where capital is city with the most population in the country.
+#7 Display countries quantity where name and local name the same.
+SELECT
+	(
+	SELECT
+		count(name)
+	FROM
+		country) - (
+	SELECT
+		count(name)
+	FROM
+		country
+	WHERE
+		name = local_name) AS Difference;
+
+	
+#8 Display cities quantity which start with A character.
+	
+	SELECT
+	count(name) AS 'Count'
+FROM
+	city
+WHERE
+	name LIKE 'a%';
+
+|Count|
++-----+
+|  260|
+
+
+#9 Display 5 cities with the most district quantity and countries in which they are.
 
 SELECT
+	city.name AS 'City',
 	country.name AS 'Country',
-	capital AS 'Capital'
+	count(district) AS 'Count'
+FROM
+	city
+JOIN country ON 
+country.code = city.country_code
+GROUP BY
+	country_code
+ORDER BY
+	count DESC
+LIMIT 5;
+
+|City           |Country      |Count|
++---------------+-------------+-----+
+|Shanghai       |China        |  363|
+|Mumbai (Bombay)|India        |  341|
+|New York       |United States|  274|
+|São Paulo      |Brazil       |  250|
+|Tokyo          |Japan        |  248|
+
+	
+#10 Display quantity each government forms and surface area they occupy. 
+
+SELECT
+	government_form AS 'Form',
+	count(*) AS 'Quantity',
+	sum(surface_area) AS 'Area'
 FROM
 	country
-JOIN city ON
-	country.code = city.country_code
-WHERE
-	country.capital =
-SELECT
-	country_code ;
+GROUP BY
+	government_form
+ORDER BY
+	Area DESC;
+
+|Form                                        |Quantity|Area       |
++--------------------------------------------+--------+-----------+
+|Federal Republic                            |      15|46116599.00|
+|Republic                                    |     122|45376058.00|
+|Constitutional Monarchy, Federation         |       4|18072106.00|
+|Co-administrated                            |       1|13120000.00|
+|Peoples Republic                            |       1| 9572900.00|
+|Constitutional Monarchy                     |      29| 4207458.50|
+|Islamic Republic                            |       2| 4154008.00|
+|Monarchy                                    |       5| 2225704.00|
+|Part of Denmark                             |       2| 2167489.00|
+|Socialistic State                           |       1| 1759540.00|
+|Islamic Emirate                             |       1|  652090.00|
+|Socialistic Republic                        |       3|  563088.00|
+|Monarchy (Sultanate)                        |       2|  315265.00|
+|Occupied by Marocco                         |       1|  266000.00|
+|Overseas Department of France               |       4|   95317.00|
+|Emirate Federation                          |       1|   83600.00|
+|Dependent Territory of Norway               |       2|   62481.00|
+|Federation                                  |       1|   41284.00|
+|Nonmetropolitan Territory of France         |       4|   30555.00|
+|Constitutional Monarchy (Emirate)           |       1|   17818.00|
+|Dependent Territory of the UK               |      12|   17619.00|
+|Administrated by the UN                     |       1|   14874.00|
+|Commonwealth of the US                      |       2|    9339.00|
+|Autonomous Area                             |       1|    6257.00|
+|Parlementary Monarchy                       |       1|    2831.00|
+|US Territory                                |       3|    1095.00|
+|Special Administrative Region of China      |       2|    1093.00|
+|Nonmetropolitan Territory of The Netherlands|       2|     993.00|
+|Monarchy (Emirate)                          |       1|     694.00|
+|Territorial Collectivity of France          |       2|     615.00|
+|Territory of Australia                      |       4|     544.00|
+|Nonmetropolitan Territory of New Zealand    |       3|     508.00|
+|Parliamentary Coprincipality                |       1|     468.00|
+|Dependent Territory of the US               |       1|      16.00|
+|Independent Church State                    |       1|       0.40|
